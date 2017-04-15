e72fa97f363c785e6d3e517e7f167b45d1d6276f
Ishan Gulhane <ishan@Ishans-MacBook-Pro.local>
2017-04-14 17:05:06 -0700

    Update deployment files

--- scripts/install.sh
+++ scripts/install.sh
@@ -73,7 +73,7 @@ function change_nginx() {
   #statements
   IP_ADDRESS=$(bx cs workers $(bx cs clusters | grep deployed | awk '{ print $1 }') | grep deployed | awk '{ print $2 }')
   echo $IP_ADDRESS
-  sed -i '' 's/xx.xx.xx.xx/'$IP_ADDRESS'/g' ../manifests/deploy-nginx.yaml
+  sed -i '' 's/xx.xx.xx.xx/'$IP_ADDRESS'/g' deploy-nginx.yaml
 }
 
 function install_helm(){
