abce1e7a97f982b801648d20f9f8f1fbd37bef9b
Ishan Gulhane <ishan.gulhane@ibm.com>
2017-04-17 14:46:11 -0700

    Deploy to bluemix button v2.0

--- scripts/bx_login.sh
+++ scripts/bx_login.sh
@@ -9,16 +9,22 @@ fi
 
 
 if [ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]; then
-  echo "Define all required environment variables and rerun the stage."
-  exit 1
+    if [ -z "$API_KEY"]
+    then
+        echo "Define BLUEMIX_USER, BLUEMIX PASSWORD and BLUEMIX_ACCOUNT environment variables or just use the API_KEY environment variable."
+        exit 1
+    else
+        echo "Logging in using API_KEY environment variable"
+    fi
 fi
 echo "Deploy pods"
 
 echo "bx login -a $CF_TARGET_URL"
-bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
-if [ $? -ne 0 ]; then
-  echo "Failed to authenticate to Bluemix"
-  exit 1
+
+if [ -z "$API_KEY"]; then
+  bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+else
+  bx login -a "$CF_TARGET_URL" --apikey "$API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
 fi
 
 # Init container clusters
