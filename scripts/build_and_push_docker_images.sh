39d3e84cdf4ba358f209a458c4be319272c6fcf7
John Zaccone <john.zaccone@gmail.com>
2017-07-13 16:09:42 -0400

    fix script

--- scripts/build_and_push_docker_images.sh
+++ scripts/build_and_push_docker_images.sh
@@ -2,18 +2,18 @@
 
 function buildAndPushDockerImages {
   cd $1
-  docker build -t registry.ng.bluemix.net/$1/microservice-speaker .
-  docker push registry.ng.bluemix.net/$1/microservice-speaker .
+  docker build -t $2 .
+  docker push $2
   cd ..
 }
 
 if [ $# -ne 1 ]; then
-    echo "usage: ./buildAndPushDockerImages.sh [bluemix container registry namespace]
+    echo "usage: ./buildAndPushDockerImages.sh [bluemix container registry namespace]"
     exit
 fi
 
-buildAndPushDockerImages sample.microservicebuilder.web-app
-buildAndPushDockerImages sample.microservicebuilder.schedule
-buildAndPushDockerImages sample.microservicebuilder.speaker
-buildAndPushDockerImages sample.microservicebuilder.session
-buildAndPushDockerImages sample.microservicebuilder.vote
+buildAndPushDockerImages sample.microservicebuilder.web-app registry.ng.bluemix.net/$1/microservice-webapp  
+buildAndPushDockerImages sample.microservicebuilder.schedule registry.ng.bluemix.net/$1/microservice-schedule
+buildAndPushDockerImages sample.microservicebuilder.speaker registry.ng.bluemix.net/$1/microservice-speaker
+buildAndPushDockerImages sample.microservicebuilder.session registry.ng.bluemix.net/$1/microservice-session
+buildAndPushDockerImages sample.microservicebuilder.vote registry.ng.bluemix.net/$1/microservice-vote
