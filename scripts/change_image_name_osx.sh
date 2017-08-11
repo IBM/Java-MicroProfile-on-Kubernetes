89d1f72e8324c50f92edf0f5716b161cc2de0258
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-08-11 11:46:18 -0700

    update for tech-talk

--- scripts/change_image_name_osx.sh
+++ scripts/change_image_name_osx.sh
@@ -3,8 +3,12 @@ if [ $# -ne 1 ]; then
     exit
 fi
 
+export IP=$(kubectl get nodes | awk ' /Ready/ { print $1;exit }')
+
 sed -i '' s#"journeycode"#$1# manifests/deploy-schedule.yaml
 sed -i '' s#"journeycode"#$1# manifests/deploy-session.yaml
 sed -i '' s#"journeycode"#$1# manifests/deploy-speaker.yaml
 sed -i '' s#"journeycode"#$1# manifests/deploy-vote.yaml
 sed -i '' s#"journeycode"#$1# manifests/deploy-webapp.yaml
+sed -i '' s#"journeycode"#$1# manifests/deploy-nginx.yaml
+sed -i '' s#"xxx.xxx.xx.xxx"#$IP# manifests/deploy-nginx.yaml
